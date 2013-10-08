dd4be34560cebd90d2e6da13a9a923f295aa9784
Zee Spencer <zee@zeespencer.com>
2013-10-08 10:22:55 -0700

    Make the name validation a stretch

--- spec/features/guest_creates_todos_spec.rb
+++ spec/features/guest_creates_todos_spec.rb
@@ -10,6 +10,8 @@ feature 'Guest creates todo' do
     expect(page).to have_content("Take out the trash")
   end
 
+  # This is a STRETCH scenario. MOVE ON unless you've already pushed to heroku
+  # and submitted a pull request
   xscenario "it does not create the todo if they don't provide a name" do
     visit '/'
 
